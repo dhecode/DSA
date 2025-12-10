56667a0e570283d7791b76f6e9773ad0c08cfceb
UTKARSH <86353517+dhecode@users.noreply.github.com>
2025-12-11 00:35:29 +0530

    Time: 1 ms (99.76%) | Memory: 46.4 MB (37.23%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,23 +1,45 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
+    //     boolean[][] rows = new boolean[9][9];
+    //     boolean[][] cols = new boolean[9][9];
+    //     boolean[][] boxes = new boolean[9][9];
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int boxIndex = (i / 3) * 3 + (j / 3);
+    //     for (int i = 0; i < 9; i++) {
+    //         for (int j = 0; j < 9; j++) {
+    //             if (board[i][j] != '.') {
+    //                 int num = board[i][j] - '1';
+    //                 int boxIndex = (i / 3) * 3 + (j / 3);
 
-                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
-                        return false;
-                    }
+    //                 if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+    //                     return false;
+    //                 }
 
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+    //                 rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+    boolean rows [][] = new boolean[9][9];
+    boolean columns [][] = new boolean[9][9];
+    boolean boxes [][] = new boolean[9][9];
+         for (int i = 0; i < 9; i++) {
+           for (int j = 0; j < 9; j++) {
+            if(board[i][j]!='.')
+            {
+                int num = board[i][j]-'1';
+                int boxIndex=(i/3)*3+(j/3);
+
+                if(rows[i][num]||columns[j][num]||boxes[boxIndex][num])
+                {
+                    return false;
                 }
+                 rows[i][num] = columns[j][num] = boxes[boxIndex][num] = true;
             }
-        }
-        return true;
+    }
+         }
+         return true;
     }
 }
